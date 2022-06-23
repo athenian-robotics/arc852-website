1c68ca15d55cf5cb254cdd8d63d8ce099ff89890
MetalTurtle <dekolis1218@gmail.com>
2022-06-22 22:57:44 -0400

    Add about page and use Next Links

--- components/MyNavbar.tsx
+++ components/MyNavbar.tsx
@@ -1,27 +1,30 @@
 import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 import React from "react";
+import Link from "next/link";
 
 const MyNavbar = () => {
     return (
         <Navbar variant="dark" className="navbar-arcblue" expand="lg">
             <Container>
-                <Navbar.Brand href="/" className="brand-logotype">
-                    <img
-                        alt=""
-                        src="/favicon.ico"
-                        width="30"
-                        height="30"
-                        className="d-inline-block align-top"
-                    />
-                    {
-                        ' ATHEnIAN' // TODO: is this the right stupid capitalization?
-                    }
-                </Navbar.Brand>
+                <Link href="/">
+                    <Navbar.Brand as="a" className="brand-logotype">
+                        <img
+                            alt=""
+                            src="/favicon.ico"
+                            width="30"
+                            height="30"
+                            className="d-inline-block align-top"
+                        />
+                        {
+                            ' ATHEnIAN' // TODO: is this the right stupid capitalization?
+                        }
+                    </Navbar.Brand>
+                </Link>
                 <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
+                        <Link href="/"><Nav.Link as="a">Home</Nav.Link></Link>
+                        <Link href="/about"><Nav.Link as="a">About</Nav.Link></Link>
                         <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                             <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                             <NavDropdown.Item href="#action/3.2">
---
+++ pages/about.tsx
@@ -0,0 +1,11 @@
+import Layout from "../components/Layout";
+
+const about = () => {
+    return (
+        <Layout>
+            <p>about us page lorem ipsum</p>
+        </Layout>
+    )
+}
+
+export default about
